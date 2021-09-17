31ce22912a514a7ed2b00cd8569930aa5861f694
joaquinabal <joabal97@gmail.com>
2021-09-16 21:39:58 -0300

    ML

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;      
+
+app.use(express.static('public'));
+
+app.listen(port,()=>console.log("is running at: http://localhost:3000/"));
+
+app.get("/", function(req,res){
+    let htmlPath = path.resolve(__dirname,"./views/home.html")   
+    res.sendFile(htmlPath)                                         
+});
+app.post("/", function(req,res){
+    let htmlPath = path.resolve(__dirname,"./views/home.html") 
+    res.sendFile(htmlPath)                                        
+});
+
+app.get("/register", function(req,res){
+    let htmlPath = path.resolve(__dirname,"./views/register.html")   
+    res.sendFile(htmlPath)                                       
+});
+
+app.get("/login", function(req,res){
+    let htmlPath = path.resolve(__dirname,"./views/login.html")   
+    res.sendFile(htmlPath)                                       
+});
