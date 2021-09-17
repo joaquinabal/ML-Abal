78474850b4a9e4bac5e28877b27a9e596dae25b5
joaquinabal <joabal97@gmail.com>
2021-09-16 21:53:53 -0300

    cambiando port

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 3000;      
+     
 
 app.use(express.static('public'));
 
-app.listen(port,()=>console.log("is running at: http://localhost:3000/"));
+app.listen(process.env.PORT || 3000,()=>console.log("is running at: http://localhost:3000/"));
 
 app.get("/", function(req,res){
     let htmlPath = path.resolve(__dirname,"./views/home.html")   
